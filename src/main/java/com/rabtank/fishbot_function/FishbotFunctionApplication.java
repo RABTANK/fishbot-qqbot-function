bb9065bac70c85929bdf6c74b24c60654317e9eb
RABTANK <1300866375@qq.com>
2025-02-20 11:30:48 +0800

    init

---
+++ src/main/java/com/rabtank/fishbot_function/FishbotFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.rabtank.fishbot_function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishbotFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishbotFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabtank/fishbot_function/FishbotFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabtank.fishbot_function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishbotFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
