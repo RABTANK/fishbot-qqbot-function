ae6d1d3ca46fa769ee17061651af630ab5e5fa10
RABTANK <1300866375@qq.com>
2025-03-03 16:11:47 +0800

    ok

--- src/main/java/com/rabtank/fishbot_function/service/MdTestService.java
+++ src/main/java/com/rabtank/fishbot_function/service/MdTestService.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
 @Service
-public class HelloService implements CommandService {
+public class MdTestService implements CommandService{
 
     @Override
     public List<String> execute(List<String> args, String userUnionId) {
         System.out.println(args);
         List<String> backList = new ArrayList<>();
-        backList.add("text");
-        backList.add("hello!" + userUnionId);
+        backList.add("markdown");
+        backList.add("{\"custom_template_id\":\"\n" + "102074147_1740987502\",\"params\":[{\"key\":\"location1\",\"values\":[\"练习场\"]},{\"key\":\"location2\",\"values\":[\"练习场\"]},{\"key\":\"location3\",\"values\":[\"练习场\"]},{\"key\":\"location4\",\"values\":[\"练习场\"]}]}");
         backList.add("yes");
         backList.add("102074147_1740986528");
         return backList;
     }
+    
 }
